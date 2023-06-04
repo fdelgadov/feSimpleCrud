46a187cfd00c2b252385913e84236abb677d33e7
fdelgadov <fdelgadov@unsa.edu.pe>
2023-06-04 09:57:19 -0500

    Se importa bootstrap y se crea el componente catalogos

---
+++ src/app/catalogos/catalogos.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CatalogosComponent } from './catalogos.component';
+
+describe('CatalogosComponent', () => {
+  let component: CatalogosComponent;
+  let fixture: ComponentFixture<CatalogosComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CatalogosComponent]
+    });
+    fixture = TestBed.createComponent(CatalogosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
