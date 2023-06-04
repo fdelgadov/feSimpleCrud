96da6e4b8916cd983094db9ca4ced70e8ea29666
fdelgadov <fdelgadov@unsa.edu.pe>
2023-06-04 10:01:46 -0500

    Se generan correctamente el component y service catalogos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CatalogosComponent } from './catalogos/catalogos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogosComponent
   ],
   imports: [
     BrowserModule
